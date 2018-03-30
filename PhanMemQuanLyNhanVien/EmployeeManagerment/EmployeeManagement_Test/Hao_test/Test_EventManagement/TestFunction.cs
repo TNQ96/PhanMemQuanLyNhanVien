1ddb0cdf352a63bc2254c1b92870320bfc5f9cab
TNQ96 <trannhatquang96ver2@gmail.com>
2018-03-30 10:24:09 +0700

    test account

---
+++ PhanMemQuanLyNhanVien/EmployeeManagerment/EmployeeManagement_Test/Hao_test/Test_EventManagement/TestFunction.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeManagement_Service.ModelDBContext;
+using EmployeeManagement_Service.Service.Module;
+namespace EmployeeManagement_Test.Hao_test.Test_EventManagement
+{
+    [TestClass]
+    public class TestFuntion
+    {
+        private PdbBonusSalary TestSalary;
+        private PdbStaff Test_staff;
+        [TestInitialize]
+        public void Setup()
+        {
+            TestSalary = new PdbBonusSalary();
+            TestSalary.IDBS = Guid.NewGuid();
+            TestSalary.IDStaff = new Guid("CFACFF13-F9D6-44D4-8ADE-F5B22FB627C1");
+            TestSalary.MoneyBonus = 4000;
+            TestSalary.DayBonus = DateTime.Now;
+            TestSalary.ReasonBonus = "i don't know";
+            TestSalary.MonthBonus = "12";
+            TestSalary.YearBonus = "2017";
+
+            Test_staff = new PdbStaff();
+            Test_staff.ID_Staff = Guid.NewGuid();
+            Test_staff.FirstName = "Hao";
+            Test_staff.LastName = "Nguyen";
+            Test_staff.Birthday = DateTime.Now;
+            Test_staff.IndentityCard = "khongbiet";
+            Test_staff.Date_Create_IC = new DateTime(2010, 12, 01);
+            Test_staff.Place_Create_IC = "campuchia";
+            Test_staff.Hometown = "Campuchia";
+            Test_staff.Phone = "01868323274";
+            Test_staff.AddressNumber = "23";
+            Test_staff.AddressStreet = "Lê văn sỹ";
+            Test_staff.AddressDistrict = "3";
+            Test_staff.AddressWard = "tphcm";
+            Test_staff.AddressCity = "Tphcm";
+            Test_staff.Sex = "nam";
+            Test_staff.Department = "Kế toán";
+            Test_staff.Position = "Kế toán trưởng";
+            Test_staff.SalaryBasic = 123;
+            Test_staff.CoefficientsSalary = 123;
+            Test_staff.isMarried = true;
+            Test_staff.DateStartWork = new DateTime(2010, 12, 03);
+            Test_staff.DateEndWork = new DateTime(2015, 12, 31);
+            Test_staff.Image = new byte[] { 0x00, 0x00, 0x00, 0x01 };
+            Test_staff.Produce = "abc";
+            Test_staff.Email = "aptran17@gmail.com";
+
+        }
+
+        [TestMethod]
+        public void TestSalaryAdd()
+        {
+            bool check = new Salary(new EmployeeManagementDBContext()) { }.Add(TestSalary);
+            Assert.AreNotEqual(check, false);
+        }
+
+        [TestMethod]
+        public void TestSalaryEdit()
+        {
+            TestSalary.IDBS = new Guid("72F22668-143F-4F20-B083-EABC9CEAA3AA");
+            TestSalary.ReasonBonus = "Edit";
+            bool check = new Salary(new EmployeeManagementDBContext()) { }.Edit(TestSalary);
+            Assert.AreNotEqual(check, false);
+        }
+
+        [TestMethod]
+        public void TestSalaryDelete()
+        {
+            bool check = new Salary(new EmployeeManagementDBContext()) { }.Delete(new Guid("C099B97F-AEBD-4F2B-96C6-6029A69417DA"));
+            Assert.AreNotEqual(check, false);
+        }
+
+        [TestMethod]
+        public void Staff_add()
+        {
+            bool check = new Staffs(new EmployeeManagementDBContext()) { }.Create(Test_staff);
+            Assert.AreNotEqual(check, false);
+        }
+
+        [TestMethod]
+        public void staff_edit()
+        {
+            PdbStaff Staff = new PdbStaff();
+            Staff = Test_staff;
+            Staff.ID_Staff = new Guid("5a1c6fe2-c0b3-4939-ab6f-0b6e0b534775");
+
+            bool check = new Staffs(new EmployeeManagementDBContext()) { }.Update(Test_staff);
+            Assert.AreNotEqual(check, false);
+        }
+        [TestMethod]
+        public void Staff_Delete()
+        {
+            Guid guid = new Guid("5a1c6fe2-c0b3-4939-ab6f-0b6e0b534775");
+            bool check = new Staffs(new EmployeeManagementDBContext()) { }.Delete(guid);
+            Assert.AreNotEqual(check, false);
+        }
+        [TestMethod]
+        public void Staff_Get()
+        {
+            Guid guid = new Guid("5a1c6fe2-c0b3-4939-ab6f-0b6e0b534775");
+            //Test_Contract.IDContract = new Guid("8E0DC287-5F62-4D61-BB26-686FF959C62A");
+            PdbStaff check = new Staffs(new EmployeeManagementDBContext()) { }.GetStaff(guid);
+            Assert.AreNotEqual(check, false);
+        }
+    }
+}
---
+++ PhanMemQuanLyNhanVien/EmployeeManagerment/EmployeeManagement_Test/Quang_Test/Test_EventManagement/TestFunction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeManagement_Service.ModelDBContext;
+using EmployeeManagement_Service.Service.Module;
+
+namespace EmployeeManagement_Test.Quang_Test.Test_EventManagement
+{
+   
+    [TestClass]
+    public class TestFunctionAccount
+    {
+        private PdbAccount account;
+        [TestInitialize]
+        public void SetUp()
+        {
+            account = new PdbAccount();
+            account.AccountName = "test1";
+            account.AccountPassword = "test";
+            account.AccountLevel = "NS";
+            account.IDAccount = Guid.NewGuid();
+            account.IDStaff = new Guid("CFACFF13-F9D6-44D4-8ADE-F5B22FB627C1");
+            account.isActive = true;
+        }
+
+        [TestMethod]
+        public void CreateAccount()
+        {
+            bool checkadd = new Accounts(new EmployeeManagementDBContext()) { }.Create(account);
+            Assert.AreNotEqual(checkadd, false);
+        }
+        [TestMethod]
+        public void UpdateAccount()
+        {
+            account = new PdbAccount();
+            account.AccountName = "testupdate";
+            account.AccountPassword = "testupdate";
+            account.AccountLevel = "Bao Ve";
+            account.IDAccount = new Guid("7B17E386-AA4B-496E-89A7-5585FF9480CE");
+            account.IDStaff = new Guid("CFACFF13-F9D6-44D4-8ADE-F5B22FB627C1");
+            account.isActive = true;
+            bool checkupdate = new Accounts(new EmployeeManagementDBContext()) { }.Update(account);
+            Assert.AreNotEqual(checkupdate, false);
+        }
+        [TestMethod]
+        public void DeleteAccount()
+        {
+            account = new PdbAccount();
+            account.AccountName = "testdelete";
+            account.AccountPassword = "testdelete";
+            account.AccountLevel = "NS";
+            account.IDAccount = new Guid("6BDD9FE6-C182-4A51-9B2D-4B92ADBB620A");
+            account.IDStaff = new Guid("B5DAF9C8-AC90-49CA-A742-B419AFAB01B5");
+            account.isActive = true;
+            bool checkdetele = new Accounts(new EmployeeManagementDBContext()) { }.Delete(account.IDAccount);
+            Assert.AreNotEqual(checkdetele, false);
+        }
+    }
+}
